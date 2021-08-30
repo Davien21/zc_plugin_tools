ac69a334d56701ca13f1188423a314365599fa5c
davien21 <chidiebereekennia@gmail.com>
2021-08-30 19:48:50 +0100

    add giphy search box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-import { appData as data } from '../appData';
+import { appData as data } from "../appData";
 
 export default function Home() {
   return (
     <React.Fragment>
       <Head>
         <title>{`${data.name} - ${data.description}`}</title>
-        <meta name='description' content={data.description} />
+        <meta name="description" content={data.description} />
       </Head>
       <div className={`text-center p-12`}>
         <h1 className={`text-gray-800 text-5xl font-extrabold`}>{data.name}</h1>
@@ -16,6 +16,7 @@ export default function Home() {
         <h1>Edited by CI/CD</h1>
         <h1>Edited Again</h1>
       </div>
+      <input placeholder="Search"></input>
     </React.Fragment>
   );
 }
